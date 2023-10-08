0f646d48ae6f7ea1c43831a32cf9a83aef380d3d
Carl Bettermann <info@carlbettermann.de>
2023-10-08 12:58:37 +0200

    Grundlegende Komponenten erstellt

--- scheppern-frontend/src/app/app-routing.module.ts
+++ scheppern-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { TicketsComponent } from './views/tickets/tickets.component';
+import { ImpressumComponent } from './views/impressum/impressum.component';
+import { InfosComponent } from './views/infos/infos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tickets', component: TicketsComponent },
+  { path: 'infos', component: InfosComponent },
+
+  { path: 'impressum', component: ImpressumComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- scheppern-frontend/src/app/app.module.ts
+++ scheppern-frontend/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { environment } from '../environments/environment';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { HomeComponent } from './views/home/home.component';
+import { DjCardComponent } from './components/dj-card/dj-card.component';
+import { TicketsComponent } from './views/tickets/tickets.component';
+import { InfosComponent } from './views/infos/infos.component';
+import { KontaktComponent } from './views/kontakt/kontakt.component';
+import { ImpressumComponent } from './views/impressum/impressum.component';
+import { HeaderComponent } from './components/header/header.component';
 
 // import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 // import { provideFirestore,getFirestore } from '@angular/fire/firestore';
@@ -16,7 +23,14 @@ import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DjCardComponent,
+    TicketsComponent,
+    InfosComponent,
+    KontaktComponent,
+    ImpressumComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
