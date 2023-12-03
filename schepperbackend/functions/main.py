c688447f49cb324b07f391837b28e0972b68f443
coderitz <ms_leugers@hotmail.de>
2023-12-03 22:12:27 +0100

    Created backup

--- schepperbackend/functions/main.py
+++ schepperbackend/functions/main.py
@@ -5,6 +5,8 @@ import base64
 import json
 import uuid
 import random
+import logging
+import os
 
 from firebase_functions import https_fn, pubsub_fn, scheduler_fn, params
 from firebase_admin import firestore, initialize_app
@@ -16,15 +18,11 @@ from firebase_functions.firestore_fn import (
   Change,
   DocumentSnapshot,
 )
+from google.cloud.firestore_v1.base_query import FieldFilter
 
+from transaction_processor import get_transactions, process_relevant_transactions, filter_relevant_transactions
 from cost_controller import review_billing_event, stop_billing
-import transaction_processor
-
-import os
-
-# cred = credentials.application_default()
-# cred = credentials.Certificate("path/to/your/serviceAccountKey.json")
-# firebase_admin.initialize_app(cred)
+from mail_handling import send_confirmation_mail, send_ticket, send_repersonalization_mail
 
 initialize_app(
     options = {
@@ -35,6 +33,7 @@ initialize_app(
 
 os.environ["PROJECT_ID"] = params.PROJECT_ID.value
 
+TICKETS_COLLECTION_NAME = "tickets_temp"
 
   
 
@@ -125,7 +124,7 @@ def writeTicket2(form_data):
 @https_fn.on_call()
 def tickets_available(req: https_fn.CallableRequest): #req: https_fn.Request) -> https_fn.Response: 
     db = firestore.client()
-    ticket_orders_collection = db.collection('tickets_ordered')
+    ticket_orders_collection = db.collection(TICKETS_COLLECTION_NAME)
     count_orders_query = ticket_orders_collection.count()
     query_result = count_orders_query.get()
     amount_of_ticket_orders = query_result[0][0].value
@@ -147,11 +146,9 @@ app = initialize_app()
 # )
 
 @https_fn.on_call()
-def writeTicket(input_data):
-
-    input_data = input_data.data
-    if input_data is None: return {"error": "Invalid input"}
-
+def writeTicket(form_data):
+    input_data = form_data.data
+    if not input_data: return {"error": "Ticket with ticket ID does not exist!"}
     try:
         ticket_data = {
             'name': input_data.get('name'),
@@ -162,129 +159,160 @@ def writeTicket(input_data):
             'helper': input_data.get("helper"),
             #'helper_job_preference': input_data.get('helper_job_preference'),
             'helper_time_preference': input_data.get('timePreferences'),
-            #'ticket_id': input_data.get('ticket_id'),
         }
-        ticket_data["ticket_id"] = ""
+        ticket_id = ""#input_data.get("ticket_id")
         ticket_data["helper_job_preference"] = ""
-        
+            
     except Exception:
         return {"error": "Input could not be parsed."}
 
-    ticket_data["modified_at"] = datetime.now()
-
     db = firestore.client()
-    if ticket_data["ticket_id"] == "":
-        ticket_id = str(uuid.uuid4())
-        
-        # determine price for ticket whether it is for helpers or for regulars
-        ticket_price_doc = db.collection("prices").document(
-            'current_helper_price' if ticket_data['helper'] else 'current_regular_price'
-        ).get()
-        
+    tickets_collection_ref = db.collection(TICKETS_COLLECTION_NAME)
 
-        if ticket_price_doc.exists:
-            ticket_price = ticket_price_doc.to_dict()["value"]
-        else:
-            return {"error": "Ticket price could not be determined."}
-        
-        ticket_data["order_id"] = str(random.randint(100000,999999)) # perhaps check if ticket id already in database
-        ticket_data["price"] = ticket_price
-        ticket_data.pop("ticket_id")
+    if ticket_id == "":        
+        # New ticket has been ordered
+        price_doc_name = "current_helper_price" if ticket_data["helper"]==True else "current_regular_price"
+        ticket_price_doc = db.collection("prices").document(price_doc_name).get()
+        if not ticket_price_doc.exists: return {"error": "Ticket price could not be determined."}
+        price = ticket_price_doc.to_dict()["value"]
 
-        new_ticket_ref = db.collection("tickets_ordered").document(ticket_id)
-        new_ticket_ref.set(ticket_data)
-
+        tickets_collection_ref.document().set({
+            **ticket_data,
+            "modified_at": datetime.now(),
+            "order_id": str(random.randint(100000,999999)), #TODO: Check if already present in orders
+            "status": "ordered",
+            "price": price
+        })
+        send_confirmation_mail(ticket_data["name"], ticket_data["email"], price)
         return "New ticket order created successfully."
 
-    else:
-        # get data of existing ticket in "payed" collection (where it needs to be as otherwise there cannot be a
-        # repersonalization)
-        existing_ticket_ref = db.collection("tickets_payed").document(ticket_data["ticket_id"])
-        existing_ticket_doc = existing_ticket_ref.get()
-        # remove the ticket id so that it is not written into the document in firestore
-        ticket_data.pop("ticket_id")
-
-        if existing_ticket_doc.exists:
-            existing_ticket = existing_ticket_doc.to_dict()
-            # data that is not present in the input from the frontend but needs to be kept
-            ticket_data["price"] = existing_ticket["price"]
-            ticket_data["order_id"] = existing_ticket["order_id"]
-
-            if existing_ticket["email"] == ticket_data["email"]:
-                # repersonalization for same person, update in the same collection 
-                existing_ticket_ref.set(ticket_data)
-                return "Ticket repersonalized successfully."
-            else:
-                # repersonailzation for new person, new ticket needs to created in pending, different one in pending
-                # with same order id must be removed
-                other_pending_repersonalizations = db.collection("tickets_pending").where(filter=FieldFilter(
-                    "order_id", "==", ticket_data["order_id"])).stream()
-                for doc in other_pending_repersonalizations:
-                    db.collection("tickets_pending").document(doc.id).delete()
-
-                pending_ticket_id = str(uuid.uuid4())
-                pending_ticket_ref = db.collection("tickets_pending").document(pending_ticket_id)
-                pending_ticket_ref.set(ticket_data)
-
-                return "New pending ticket created successfully."
-        else:
-            return {"error": "Ticket with ticket ID does not exist!"}
-
-
-@https_fn.on_call()
-def newTicketConfirmed(input_data):
-    db = firestore.client()
-
-    ticket_data = input_data.data
-    ticket_id = ticket_data["ticket_id"]
-
-    pending_ticket_ref = db.collection("tickets_pending").document(ticket_id)
-    pending_ticket_doc = pending_ticket_ref.get()
-
-    if pending_ticket_doc.exists:
-        # Delete the existing ticket with the same order id from tickets_payed
-        pending_ticket = pending_ticket_doc.to_dict()
-        payed_tickets_with_same_order_id = list(db.collection("tickets_pending").where(filter = FieldFilter(
-            "order_id", "==", pending_ticket["order_id"]
-        )).stream())
+    # Existing ticket is to be repersonalized
+    existing_ticket_ref = tickets_collection_ref.document(ticket_id)
+    existing_ticket_doc = existing_ticket_ref.get()
+    if not existing_ticket_doc.exists: return {"error": "Ticket with ticket ID does not exist!"}
+            
+    existing_ticket = existing_ticket_doc.to_dict()
+    if existing_ticket["status"] != "payed": return {"error": "Ticket has not been payed yet!"}
         
-        amount_of_payed_tickets_with_same_order_id = len(payed_tickets_with_same_order_id)
-        if amount_of_payed_tickets_with_same_order_id == 0:
-            return {"error": "Payed ticket with order ID of pending ticket does not exist!"}
-        elif amount_of_payed_tickets_with_same_order_id > 1:
-            return {"error": "More than one payed ticket with order ID of pending ticket exists!"}
-        else:
-            # If there is exactly one ticket with the order ID of the pending one, delete it.
-            db.collection("tickets_payed").document(payed_tickets_with_same_order_id[0].id).delete()
-            # Create new ticket in tickets_payed from the one in tickets_pending
-            db.collection("tickets_payed").document(ticket_id).set(pending_ticket)
-            # Delete the ticket from pending tickets
-            db.collection("tickets_pending").document(ticket_id).delete()
+    if existing_ticket["email"] == ticket_data["email"]:
+        existing_ticket_ref.set({
+            **ticket_data,
+            "modified_at": datetime.now(),
+            "order_id": existing_ticket["order_id"],
+            "status": existing_ticket["status"],
+            "price": existing_ticket["price"]
+        })
+        if (existing_ticket["name"] != ticket_data["name"]) or (existing_ticket["surname"] != ticket_data["surname"]):
+            # If the name changed, send another ticket.
+            send_ticket(ticket_id, ticket_data["name"], ticket_data["surname"], ticket_data["email"],
+                        ticket_data["order_id"], price)
 
-            return "New ticket created from pending ticket successfully."
-    else:
-        return {"error": "Pending ticket with ticket ID does not exist!"}
+        return "Ticket repersonalized successfully."
+    
+    # E-Mail has been changed, so create new pending ticket.
+    other_pending_repersonalizations = tickets_collection_ref.where(
+        filter=FieldFilter("status", "==", "pending")).where(
+        filter=FieldFilter("order_id", "==", existing_ticket["order_id"])).stream()
+    [tickets_collection_ref.document(doc.id).delete() for doc in other_pending_repersonalizations]
 
-
-
-@scheduler_fn.on_schedule(schedule="0 18 * * *")
-def daily_trigger_test(event: scheduler_fn.ScheduledEvent) -> None:
-    print("Executed successfully")
-
+    _, pending_ticket_ref = tickets_collection_ref.add({
+        **ticket_data,
+        "modified_at": datetime.now(),
+        "order_id": existing_ticket["order_id"],
+        "status": "pending",
+        "price": existing_ticket["price"]
+    })
+    send_repersonalization_mail(ticket_id=pending_ticket_ref.id, 
+                                old_name=existing_ticket["name"], 
+                                new_name=ticket_data["name"], 
+                                email=ticket_data["email"])
+            
+    return "New pending ticket created successfully."
+            
 
 @https_fn.on_call()
-def process_transactions(req: https_fn.CallableRequest):
-    transactions = transaction_processor.get_transactions()
+def newTicketConfirmed(form_data):
+    ticket_data = form_data.data
+
+    db = firestore.client()
+    tickets_collection_ref = db.collection(TICKETS_COLLECTION_NAME)
+    
+    pending_ticket_doc = tickets_collection_ref.document(ticket_data["ticket_id"]).get()
+    if not pending_ticket_doc.exists: return {"error": "Pending ticket not found!"}
+
+    pending_ticket = pending_ticket_doc.to_dict()
+    if pending_ticket["status"] != "pending": return {"error": "Ticket status is not pending!"}
+    
+    payed_tickets_with_same_order_id = list(tickets_collection_ref.where(
+        filter=FieldFilter("status", "==", "payed")).where(
+        filter=FieldFilter("order_id", "==", pending_ticket["order_id"]))
+        .stream())
+        
+    if len(payed_tickets_with_same_order_id) == 0:
+        return {"error": "Payed ticket with order ID of pending ticket does not exist!"}
+    elif len(payed_tickets_with_same_order_id) > 1:
+        return {"error": "More than one payed ticket with order ID of pending ticket exists!"}
+    else:
+        # Delete the payed ticket from the collection so that a new one must be created, so that the ticket id changes,
+        # so that repersonalization does not work anymore for the old person.
+        tickets_collection_ref.document(payed_tickets_with_same_order_id[0].id).delete()
+        pending_ticket["status"] = "payed"
+        tickets_collection_ref.document(ticket_data["ticket_id"]).set(pending_ticket)
+        send_ticket(ticket_id=ticket_data["ticket_id"], 
+                    name=pending_ticket["name"], 
+                    surname=pending_ticket["surname"], 
+                    email=pending_ticket["email"], 
+                    order_id=pending_ticket["order_id"],
+                    price=pending_ticket["price"])
+        
+        return "New ticket created from pending ticket successfully."
+
+
+@https_fn.on_request()
+def validate_ticket(req: https_fn.Request) -> https_fn.Response:
+    ticket_id = req.query_string.decode("utf-8")
+    
+    db = firestore.client()
+    doc_ref = db.collection(TICKETS_COLLECTION_NAME).document(ticket_id)
+    doc = doc_ref.get()
+    
+    if not doc.exists: return f"Ticket mit id {ticket_id} konnte nicht gefunden werden."
+    data = doc.to_dict()
+    if data["status"] != "payed": return "Ticket wurde noch nicht bezahlt!"
+
+    return f'{data["name"]} {data["surname"]}, Bestellnummer: {data["order_id"]}'
+    
+        
+
+@https_fn.on_request()
+def process_transactions(req: https_fn.Request) -> https_fn.Response:
+    transactions = get_transactions()
     (
         transactions_correct_amount_correct_id,
         transactions_correct_amount_false_id,
         transactions_false_amount_correct_id
-    ) = transaction_processor.filter_relevant_transactions(transactions)
+    ) = filter_relevant_transactions(transactions)
 
-    transaction_processor.process_relevant_transactions(transactions_correct_amount_correct_id, 
-        transactions_correct_amount_false_id, transactions_false_amount_correct_id)
+    process_relevant_transactions(transactions_correct_amount_correct_id, transactions_correct_amount_false_id,
+                                  transactions_false_amount_correct_id)
     
     print("Processing transactions finished")
+
+    return "ok"
+
+
+@scheduler_fn.on_schedule(schedule="0 10 * * *")
+def order_lifecycle_handler(event: scheduler_fn.ScheduledEvent) -> None:
+    db = firestore.client()
+    for ticket_order_doc in db.collection(TICKETS_COLLECTION_NAME).where(filter=FieldFilter("status", "==", "ordered")).stream():
+        ticket_order = ticket_order_doc.to_dict()
+        order_date = ticket_order["modified_at"]
+        if (order_date is None) or (order_date==""): logging.exception("Order date is invalid.")
+        
+
+@scheduler_fn.on_schedule(schedule="0 18 * * *")
+def daily_trigger_test(event: scheduler_fn.ScheduledEvent) -> None:
+    print("Executed successfully")
 
 
 @pubsub_fn.on_message_published(topic="project_cost")
